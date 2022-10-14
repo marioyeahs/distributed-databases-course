d1d5aec6c71bf24ef13fb74ba4aa22b1e2d5edad
Mario Cervantes <mgallardoc1800@alumno.ipn.mx>
2022-10-14 16:04:23 -0500

    add: query to linked server

--- linked_server.sql
+++ linked_server.sql
@@ -10,3 +10,5 @@ crear stored procedure O TRIGGER para registrar una orden (numero de oreden, ID 
 schema.production, en maquina conrtraria (PRODUCT ID)
 schema.sales, desde propia máquina (SALES ORDER ID)
 */
+
+SELECT * FROM OPENQUERY (LINKED_SERVER, 'SELECT product_id FROM production.product');
